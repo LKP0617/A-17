1bbdeb2d0dfd2846cd7fec796d0ca9f037993878
廖冠博 <h2itwh2itw@gmail.com>
2024-10-29 22:46:36 +0800

    index fix

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
+import './assets/scss/layout/standard.scss';/*讓我放最下面覆蓋 */
 
 
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 
-
 /*cliff*/
-import './assets/scss/layout/bootstrap.min.css';/*讓我放最下面覆蓋 */
-import './assets/scss/layout/standard.scss';/*讓我放最下面覆蓋 */
+// import './assets/scss/layout/bootstrap.min.css';/*讓我放最下面覆蓋 */
 import './assets/scss/layout/header.js';/*讓我放最下面覆蓋 */
 import './assets/scss/layout/_header.scss';/*讓我放最下面覆蓋 */
 import './assets/scss/layout/_footer.scss';/*讓我放最下面覆蓋 */
