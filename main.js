c58b2fd9feabea4876336fb7c6d1da23fe11a2fd
cliffwu <cliff104017023@gmail.com>
2024-10-29 15:52:08 +0800

    cliff-上傳header、footer、story.html

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+/*cliff*/
+import './assets/scss/layout/bootstrap.min.css';
+import './assets/scss/layout/standard.scss';
+import './assets/scss/layout/header.js';
+import './assets/scss/layout/_header.scss';
+import './assets/scss/layout/_footer.scss';
+import './assets/scss/story.scss';
+/*cliff*/
+
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
