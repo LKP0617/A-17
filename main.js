177da9a2f317919e7d266cf4f855e44f8b4f031f
cliffwu <cliff104017023@gmail.com>
2024-10-30 15:44:49 +0800

    header加入註冊/登入彈窗--尚未完成樣式

--- main.js
+++ main.js
@@ -7,8 +7,12 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 
 /*cliff*/
 // import './assets/scss/layout/bootstrap.min.css';/*讓我放最下面覆蓋 */
-import './assets/scss/layout/header.js';/*讓我放最下面覆蓋 */
+// import 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css';
+import 'https://code.jquery.com/jquery-3.5.1.slim.min.js';
+import 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js';
+import 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js';
 import './assets/scss/layout/_header.scss';/*讓我放最下面覆蓋 */
+import './assets/scss/layout/header.js';/*讓我放最下面覆蓋 */
 import './assets/scss/layout/_footer.scss';/*讓我放最下面覆蓋 */
 import './assets/scss/story.scss';
 /*cliff*/
