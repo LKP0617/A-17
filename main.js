0e04bf4330bdcaa74810e913d1a581ad69fd5e0b
cliffwu <cliff104017023@gmail.com>
2024-10-30 15:48:01 +0800

    test合併問題

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 
 
 /*cliff*/
-// import './assets/scss/layout/bootstrap.min.css';/*讓我放最下面覆蓋 */
+import './assets/scss/layout/bootstrap.min.css';/*讓我放最下面覆蓋 */
 // import 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css';
 import 'https://code.jquery.com/jquery-3.5.1.slim.min.js';
 import 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js';
