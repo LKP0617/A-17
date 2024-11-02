8eeabde38ade295de6b320b304a3b1b65c92db4a
cliffwu <cliff104017023@gmail.com>
2024-11-03 02:28:53 +0800

    test

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+import './assets/scss/layout/standard.scss';/*讓我放最下面覆蓋*/
 
 
 import './assets/scss/all.scss';
@@ -12,7 +12,7 @@ import 'https://code.jquery.com/jquery-3.5.1.slim.min.js';
 import 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js';
 import 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js';
 import 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js';
-import './assets/scss/layout/standard.scss';/*讓我放最下面覆蓋*/
+
 import './assets/scss/layout/_header.scss';/*讓我放最下面覆蓋 */
 import './assets/scss/layout/header.js';/*讓我放最下面覆蓋 */
 import './assets/scss/layout/_footer.scss';/*讓我放最下面覆蓋 */
