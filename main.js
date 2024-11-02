ba0731b023b970004f936ef5c753ac005d53866d
cliffwu <cliff104017023@gmail.com>
2024-11-03 03:16:00 +0800

    test2

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import './assets/scss/layout/standard.scss';/*讓我放最下面覆蓋*/
 
 
 import './assets/scss/all.scss';
-import 'bootstrap/dist/js/bootstrap.min.js';
+// import 'bootstrap/dist/js/bootstrap.min.js';
 
 
 /*cliff*/
@@ -14,9 +14,9 @@ import 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js';
 import 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js';
 
 import './assets/scss/layout/_header.scss';/*讓我放最下面覆蓋 */
-import './assets/scss/layout/header.js';/*讓我放最下面覆蓋 */
 import './assets/scss/layout/_footer.scss';/*讓我放最下面覆蓋 */
 import './assets/scss/story.scss';
+import './assets/scss/layout/header.js';/*讓我放最下面覆蓋 */
 
 
 /*cliff*/
