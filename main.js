17e40b006fa7b2a93f2bf8b8ede3273bae02d039
cliffwu <cliff104017023@gmail.com>
2024-11-03 02:00:11 +0800

    footer test

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import './assets/scss/layout/standard.scss';/*讓我放最下面覆蓋*/
+
 
 
 import './assets/scss/all.scss';
@@ -11,13 +11,13 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 import 'https://code.jquery.com/jquery-3.5.1.slim.min.js';
 import 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js';
 import 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js';
-
+import 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js';
+import './assets/scss/layout/standard.scss';/*讓我放最下面覆蓋*/
 import './assets/scss/layout/_header.scss';/*讓我放最下面覆蓋 */
 import './assets/scss/layout/header.js';/*讓我放最下面覆蓋 */
 import './assets/scss/layout/_footer.scss';/*讓我放最下面覆蓋 */
 import './assets/scss/story.scss';
 
 
-// import './assets/scss/layout/question.scss';
 /*cliff*/
 console.log("Hello world!");
